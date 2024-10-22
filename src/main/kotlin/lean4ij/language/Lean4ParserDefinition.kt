f95e695daba7787deec481732e6dcc410f97f39a
Shanghe Chen <onriv@outlook.com>
2024-10-23 00:48:53 +0800

    revert 3fc1678 for very weird behavior of auto complete (it removes seemly all content)

--- src/main/kotlin/lean4ij/language/Lean4ParserDefinition.kt
+++ src/main/kotlin/lean4ij/language/Lean4ParserDefinition.kt
@@ -2,65 +2,41 @@ package lean4ij.language
 
 import com.intellij.lang.ASTNode
 import com.intellij.lang.ParserDefinition
-import com.intellij.lang.PsiBuilder
 import com.intellij.lang.PsiParser
-import com.intellij.lexer.EmptyLexer
 import com.intellij.lexer.Lexer
 import com.intellij.openapi.project.Project
 import com.intellij.psi.FileViewProvider
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import com.intellij.psi.tree.IElementType
 import com.intellij.psi.tree.IFileElementType
 import com.intellij.psi.tree.TokenSet
-import com.intellij.psi.util.PsiUtilCore
-
-class EmptyParser : PsiParser {
-    /**
-     * copy from [org.jetbrains.plugins.textmate.psi.TextMateParser]
-     */
-    override fun parse(root: IElementType, builder: PsiBuilder): ASTNode {
-        val mark = builder.mark()
-        while (!builder.eof()) {
-            builder.advanceLexer()
-        }
-        mark.done(root)
-        return builder.treeBuilt
-    }
-
-}
 
 /**
- * copy from [com.intellij.openapi.fileTypes.PlainTextParserDefinition]
+ * ref https://github.com/JetBrains/intellij-community/blob/master/platform/core-impl/src/com/intellij/openapi/fileTypes/PlainTextParserDefinition.java
  */
 class Lean4ParserDefinition : ParserDefinition {
-
-    companion object {
-        val LEAN4_FILE_ELEMENT_TYPE = IFileElementType(Lean4FileType.language)
-    }
-
     override fun createLexer(project: Project?): Lexer {
-        return EmptyLexer();
+        TODO("Not yet implemented")
     }
 
     override fun createParser(project: Project?): PsiParser {
-        return EmptyParser()
+        TODO("Not yet implemented")
     }
 
     override fun getFileNodeType(): IFileElementType {
-        return LEAN4_FILE_ELEMENT_TYPE
+        TODO("Not yet implemented")
     }
 
     override fun getCommentTokens(): TokenSet {
-        return TokenSet.EMPTY;
+        TODO("Not yet implemented")
     }
 
     override fun getStringLiteralElements(): TokenSet {
-        return TokenSet.EMPTY;
+        TODO("Not yet implemented")
     }
 
     override fun createElement(node: ASTNode?): PsiElement {
-        return PsiUtilCore.NULL_PSI_ELEMENT;
+        TODO("Not yet implemented")
     }
 
     override fun createFile(viewProvider: FileViewProvider): PsiFile {
